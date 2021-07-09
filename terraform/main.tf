71880449c2a2c33b6fc6bb4c417575fb58175781
Kirill Kolchanov <k.kolchanov@gmail.com>
2021-07-09 09:14:43 +0500

    Add files for work project

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.12.31"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+  access_key = ""
+  secret_key = ""
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  env    = "devops_course"
+  az     = "eu-west-2a"
+}
+
+module "ec2" {
+  source    = "./modules/ec2"
+  subnet_id = module.vpc.public_subnet_id
+  sg_ids    = [module.vpc.public_sg_id]
+  name      = "web-host"
+  env       = "devops_course"
+  key_name  = ""
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr_block" {
+  default = "10.10.0.0/16"
+}
+
+variable "public_subnet" {
+  default = "10.10.11.0/24"
+}
+
+variable "nat_gw_subnet" {
+  default = "10.10.9.0/24"
+}
+
+variable "az" {
+  default = ""
+}
+
+variable "env" {
+  default = ""
+}
